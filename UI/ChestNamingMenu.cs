5f6d8a3f8a2ed32aa82d82ab7e72f4c50b814f17
adyingdeath <2975527435@qq.com>
2025-12-25 11:40:05 +0800

    feat: Make the controller cursor snap to the "Done" button by default.
    This change is made because initially snapping the cursor to the text box causes the left-click to fail to trigger the controller keyboard (the exact reason for this is unclear).
    As a workaround, players will now need to move the cursor manually after opening the menu, after which they can click to open the virtual keyboard.

--- UI/ChestNamingMenu.cs
+++ UI/ChestNamingMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using StardewModdingAPI;
 using StardewValley;
 using StardewValley.Menus;
 
@@ -19,47 +20,48 @@ namespace LabelChest.UI {
         private const int BUTTON_SIZE = 64;
         private const int TEXTBOX_HEIGHT = 48;
 
-        private TextBox textBox;
-        private ClickableTextureComponent cancelButton;
-        private ClickableTextureComponent doneButton;
+        public TextBox textBox;
+        public ClickableComponent textBoxCC;
+        public ClickableTextureComponent cancelButton;
+        public ClickableTextureComponent doneButton;
         private Action<string> onConfirm;
         private ChestNamingMenuTranslation translations;
 
         // Component IDs for navigation
-        private const int TEXTBOX_ID = 104;
+        private const int TEXTBOX_CC_ID = 104;
         private const int DONE_BUTTON_ID = 102;
         private const int CANCEL_BUTTON_ID = 103;
 
-        private string originalText;
+        private readonly string originalText;
 
         public ChestNamingMenu(
             ChestNamingMenuTranslation translations,
-            string defaultText,
+            string originalText,
             Action<string> onConfirm
         ) {
             this.translations = translations;
-            this.originalText = defaultText;
+            this.originalText = originalText;
             this.onConfirm = onConfirm;
 
             // Calculate window size based on content
             string title = translations.SetLabelTitle;
             Vector2 titleSize = Game1.dialogueFont.MeasureString(title);
 
-            this.width = Math.Max(400, (int)titleSize.X + 100);
-            this.width = Math.Max(this.width, TEXTBOX_WIDTH + 100);
+            width = Math.Max(400, (int)titleSize.X + 100);
+            width = Math.Max(width, TEXTBOX_WIDTH + 100);
 
-            this.height = 250;
+            height = 250;
 
             // Center on screen
-            Vector2 pos = Utility.getTopLeftPositionForCenteringOnScreen(this.width, this.height);
-            this.xPositionOnScreen = (int)pos.X;
-            this.yPositionOnScreen = (int)pos.Y;
+            Vector2 pos = Utility.getTopLeftPositionForCenteringOnScreen(width, height);
+            xPositionOnScreen = (int)pos.X;
+            yPositionOnScreen = (int)pos.Y;
 
             // Setup TextBox (Centered and wider)
-            int textBoxX = this.xPositionOnScreen + (this.width - TEXTBOX_WIDTH) / 2;
-            int textBoxY = this.yPositionOnScreen + 85;
+            int textBoxX = xPositionOnScreen + (width - TEXTBOX_WIDTH) / 2;
+            int textBoxY = yPositionOnScreen + 85;
 
-            this.textBox = new TextBox(
+            textBox = new TextBox(
                 Game1.content.Load<Texture2D>("LooseSprites\\textBox"),
                 null, Game1.smallFont, Game1.textColor
             ) {
@@ -67,112 +69,101 @@ namespace LabelChest.UI {
                 Y = textBoxY,
                 Width = TEXTBOX_WIDTH,
                 Height = TEXTBOX_HEIGHT,
-                Text = defaultText ?? ""
+                Text = originalText ?? ""
             };
 
             // Create clickable component for textbox for navigation
-            ClickableComponent textBoxCC = new ClickableComponent(
-                new Rectangle(textBoxX, textBoxY, TEXTBOX_WIDTH, TEXTBOX_HEIGHT), "") {
-                myID = TEXTBOX_ID,
+            textBoxCC = new ClickableComponent(
+                new Rectangle(textBoxX, textBoxY, (int)(TEXTBOX_WIDTH * 0.6), (int)(TEXTBOX_HEIGHT * 0.6)), "") {
+                myID = TEXTBOX_CC_ID,
                 upNeighborID = -1,
                 downNeighborID = DONE_BUTTON_ID,
-                leftNeighborID = CANCEL_BUTTON_ID,
-                rightNeighborID = DONE_BUTTON_ID,
-                fullyImmutable = true, // Use fullyImmutable = true to disable auto neighbour changes.
+                leftNeighborID = -1,
+                rightNeighborID = -1,
             };
 
             // Setup Buttons (Centered below textbox)
             int totalButtonsWidth = (BUTTON_SIZE * 2) + BUTTON_GAP;
-            int buttonsStartX = this.xPositionOnScreen + (this.width - totalButtonsWidth) / 2;
-            int buttonsY = this.yPositionOnScreen + 160;
+            int buttonsStartX = xPositionOnScreen + (width - totalButtonsWidth) / 2;
+            int buttonsY = yPositionOnScreen + 160;
 
             // Cancel Button (left)
-            this.cancelButton = new ClickableTextureComponent(
+            cancelButton = new ClickableTextureComponent(
                 new Rectangle(buttonsStartX, buttonsY, BUTTON_SIZE, BUTTON_SIZE),
                 Game1.mouseCursors,
                 new Rectangle(192, 256, 64, 64), // CANCEL Icon sprite
                 1f) {
                 myID = CANCEL_BUTTON_ID,
-                upNeighborID = TEXTBOX_ID,
+                upNeighborID = TEXTBOX_CC_ID,
                 downNeighborID = -1,
                 leftNeighborID = -1,
                 rightNeighborID = DONE_BUTTON_ID,
-                fullyImmutable = true,
             };
 
             // Done Button (right)
-            this.doneButton = new ClickableTextureComponent(
+            doneButton = new ClickableTextureComponent(
                 new Rectangle(buttonsStartX + BUTTON_SIZE + BUTTON_GAP, buttonsY, BUTTON_SIZE, BUTTON_SIZE),
                 Game1.mouseCursors,
                 new Rectangle(128, 256, 64, 64), // OK Icon sprite
                 1f) {
                 myID = DONE_BUTTON_ID,
-                upNeighborID = TEXTBOX_ID,
+                upNeighborID = TEXTBOX_CC_ID,
                 downNeighborID = -1,
                 leftNeighborID = CANCEL_BUTTON_ID,
                 rightNeighborID = -1,
-                fullyImmutable = true,
             };
 
-            // Add components to menu
-            this.allClickableComponents = new List<ClickableComponent>
-            {
-                textBoxCC,
-                this.cancelButton,
-                this.doneButton
-            };
-
-            this.textBox.SelectMe();
+            Game1.keyboardDispatcher.Subscriber = textBox;
+            //textBox.Selected = false;
 
             // Enable controller navigation
             if (Game1.options.SnappyMenus) {
-                this.snapToDefaultClickableComponent();
+                populateClickableComponentList();
+                snapToDefaultClickableComponent();
             }
         }
 
         public override void snapToDefaultClickableComponent() {
-            this.currentlySnappedComponent = this.getComponentWithID(TEXTBOX_ID);
-            if (this.currentlySnappedComponent != null) {
-                this.snapCursorToCurrentSnappedComponent();
+            currentlySnappedComponent = getComponentWithID(DONE_BUTTON_ID);
+            if (currentlySnappedComponent != null) {
+                snapCursorToCurrentSnappedComponent();
             }
         }
 
         public override void receiveGamePadButton(Buttons b) {
             base.receiveGamePadButton(b);
 
-            if (this.textBox.Selected && (
-                b == Buttons.DPadUp
-                || b == Buttons.DPadDown
-                || b == Buttons.DPadLeft
-                || b == Buttons.DPadRight
-                || b == Buttons.LeftThumbstickLeft
-                || b == Buttons.LeftThumbstickUp
-                || b == Buttons.LeftThumbstickDown
-                || b == Buttons.LeftThumbstickRight
+            SButton button = b.ToSButton();
+
+            if (textBox.Selected && (
+                button.Equals(SButton.DPadUp)
+                || button.Equals(SButton.DPadDown)
+                || button.Equals(SButton.DPadLeft)
+                || button.Equals(SButton.DPadRight)
+                || button.Equals(SButton.LeftThumbstickUp)
+                || button.Equals(SButton.LeftThumbstickDown)
+                || button.Equals(SButton.LeftThumbstickLeft)
+                || button.Equals(SButton.LeftThumbstickRight)
                 )
             ) {
-                this.textBox.Selected = false;
+                textBox.Selected = false;
+            }
+
+            if (button.Equals(SButton.ControllerB)) {
+                // Use original text for Esc
+                Confirm(originalText);
             }
         }
 
         public override void receiveLeftClick(int x, int y, bool playSound = true) {
             base.receiveLeftClick(x, y, playSound);
-            this.textBox.Update();
+            textBox.Update();
 
-            if (this.cancelButton.containsPoint(x, y)) {
+            if (cancelButton.containsPoint(x, y)) {
                 Confirm("");
             }
-            if (this.doneButton.containsPoint(x, y)) {
-                Confirm(this.textBox.Text);
-            }
-
-            // Handle textbox click
-            Rectangle textBoxBounds = new Rectangle(this.textBox.X, this.textBox.Y, this.textBox.Width, this.textBox.Height);
-            if (textBoxBounds.Contains(x, y)) {
-                this.textBox.Selected = true;
-            }
-            else {
-                this.textBox.Selected = false;
+            if (doneButton.containsPoint(x, y)) {
+                Confirm(textBox.Text);
             }
         }
 
@@ -180,11 +171,9 @@ namespace LabelChest.UI {
             if (key == Keys.Escape) {
                 // Use original text for Esc
                 Confirm(originalText);
-            }
-            else if (key == Keys.Enter) {
-                Confirm(this.textBox.Text);
-            }
-            else if (!this.textBox.Selected && !Game1.options.doesInputListContain(Game1.options.menuButton, key)) {
+            } else if (key == Keys.Enter) {
+                Confirm(textBox.Text);
+            } else if (!textBox.Selected && !Game1.options.doesInputListContain(Game1.options.menuButton, key)) {
                 base.receiveKeyPress(key);
             }
         }
@@ -199,8 +188,8 @@ namespace LabelChest.UI {
             base.performHoverAction(x, y);
 
             // Scale button on hover
-            this.cancelButton.scale = this.cancelButton.containsPoint(x, y) ? 1.1f : 1.0f;
-            this.doneButton.scale = this.doneButton.containsPoint(x, y) ? 1.1f : 1.0f;
+            cancelButton.scale = cancelButton.containsPoint(x, y) ? 1.1f : 1.0f;
+            doneButton.scale = doneButton.containsPoint(x, y) ? 1.1f : 1.0f;
         }
 
         public override void draw(SpriteBatch b) {
@@ -212,10 +201,10 @@ namespace LabelChest.UI {
                 b,
                 Game1.mouseCursors,
                 new Rectangle(384, 373, 18, 18),
-                this.xPositionOnScreen,
-                this.yPositionOnScreen,
-                this.width,
-                this.height,
+                xPositionOnScreen,
+                yPositionOnScreen,
+                width,
+                height,
                 Color.White,
                 4f
             );
@@ -227,24 +216,23 @@ namespace LabelChest.UI {
                 b,
                 title,
                 Game1.dialogueFont,
-                new Vector2(this.xPositionOnScreen + (this.width - titleSize.X) / 2, this.yPositionOnScreen + 30),
+                new Vector2(xPositionOnScreen + (width - titleSize.X) / 2, yPositionOnScreen + 30),
                 Game1.textColor
             );
 
             // Draw controls
-            this.textBox.Draw(b);
-            this.cancelButton.draw(b);
-            this.doneButton.draw(b);
+            textBox.Draw(b);
+            cancelButton.draw(b);
+            doneButton.draw(b);
 
             // Draw button tooltips on hover
-            if (this.cancelButton.containsPoint(Game1.getMouseX(), Game1.getMouseY())) {
+            if (cancelButton.containsPoint(Game1.getMouseX(), Game1.getMouseY())) {
                 drawHoverText(b, translations.CancelButton, Game1.smallFont);
-            }
-            else if (this.doneButton.containsPoint(Game1.getMouseX(), Game1.getMouseY())) {
+            } else if (doneButton.containsPoint(Game1.getMouseX(), Game1.getMouseY())) {
                 drawHoverText(b, translations.OkButton, Game1.smallFont);
             }
 
-            this.drawMouse(b);
+            drawMouse(b);
         }
     }
 }
