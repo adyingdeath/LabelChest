403dc8dde56306a720c032b3a38ac20a2d8456a0
adyingdeath <2975527435@qq.com>
2025-12-28 16:40:03 +0800

    feat: config menu can scroll now.

--- UI/ConfigMenu.cs
+++ UI/ConfigMenu.cs
@@ -7,7 +7,13 @@ using StardewValley.Menus;
 namespace LabelChest.UI;
 
 public class ConfigMenu : OptionsPage {
-    public ConfigMenu() : base(0, 0, Game1.viewport.Width, Game1.viewport.Height) {
+    private const int MARGIN_X = 100;
+    private const int MARGIN_Y = 100;
+    private const int PADDING_X = 20;
+    private const int PADDING_Y = 20;
+    private const int SPACE_Y = 8;
+    private const int PREVIEW_WIDTH = 200;
+    public ConfigMenu() : base(MARGIN_X, MARGIN_Y, Game1.viewport.Width - MARGIN_X * 2, Game1.viewport.Height - MARGIN_Y * 2) {
         options = new List<OptionsElement> {
             new ConfigSlider("Test", (value) => {
                 ModEntry.Log($"{value}");
@@ -21,9 +27,24 @@ public class ConfigMenu : OptionsPage {
             .AddOption("Jungle-value", "Jungle")
         };
 
+        for (int i = 0;i < 30;i++) {
+            int current = i;
+            options.Add(new ConfigCheckbox($"Checkbox {i}", (value) => {
+                ModEntry.Log($"{current}: {value}");
+            }));
+        }
+
         optionSlots.Clear();
         for (int i = 0; i < 7; i++) {
-            optionSlots.Add(new ClickableComponent(new Rectangle(100, 100 + i * 50, 300, 50), i.ToString() ?? "") {
+            optionSlots.Add(new ClickableComponent(
+                new Rectangle(
+                    xPositionOnScreen + PADDING_X + PREVIEW_WIDTH,
+                    yPositionOnScreen + PADDING_Y + i * ((height - 2 * PADDING_Y - 6 * SPACE_Y) / 7) + SPACE_Y * Math.Max(0, i - 0),
+                    width - 2 * MARGIN_X - 2 * PADDING_X - PREVIEW_WIDTH,
+                    (height - 2 * PADDING_Y - 6 * SPACE_Y) / 7
+                ),
+                i.ToString() ?? ""
+            ) {
                 myID = i,
                 downNeighborID = (i < 6) ? (i + 1) : (-7777),
                 upNeighborID = (i > 0) ? (i - 1) : (-7777),
@@ -41,10 +62,10 @@ public class ConfigMenu : OptionsPage {
             b,
             Game1.mouseCursors,
             new Rectangle(384, 373, 18, 18),
-            50,
-            50,
-            Game1.viewport.Width - 100,
-            Game1.viewport.Height - 100,
+            MARGIN_X,
+            MARGIN_Y,
+            width,
+            height,
             Color.White,
             4f
         );
